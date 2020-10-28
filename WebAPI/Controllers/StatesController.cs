abcc1be10c8e0afe77d72c80f6325ccb518edb52
Osinnowo Emmanuel <liquidcoding2009@gmail.com>
2020-10-28 05:51:47 +0100

    Implementation of IStateAppService -> StateAppService

--- WebAPI/Controllers/StatesController.cs
+++ WebAPI/Controllers/StatesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AppService.AppModel.ViewModel;
 using AppService.Repository.Abstractions;
 using Core.Model;
 using Microsoft.AspNetCore.Authorization;
@@ -20,26 +21,38 @@ namespace WebAPI.Controllers
         private readonly IUserService _userService;
         private readonly UserManager<AppUser> _userManager;
         private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IStateAppService _stateAppService;
 
-        public StateController(IUserService userService, UserManager<AppUser> userManager, IHttpContextAccessor httpContextAccessor)
+        public StateController(IUserService userService,
+            UserManager<AppUser> userManager,
+            IHttpContextAccessor httpContextAccessor,
+            IStateAppService stateAppService)
         {
             _userService = userService;
             _userManager = userManager;
             _httpContextAccessor = httpContextAccessor;
+            _stateAppService = stateAppService;
         }
 
-        [HttpPost]
+        [HttpGet]
         [Route("api/states")]
         public IActionResult GetAllStates()
         {
-            return Ok();
+            return Ok(ResponseViewModel.Ok(_stateAppService.GetAllStates()));
         }
 
-        [HttpPost]
-        [Route("api/state/{id}")]
+        [HttpGet]
+        [Route("api/state/{stateId}")]
         public IActionResult GetStateBy(int stateId)
         {
-            return Ok();
+            return Ok(ResponseViewModel.Ok(_stateAppService.GetStateByIts(stateId)));
+        }
+
+        [HttpGet]
+        [Route("api/state/{stateName}")]
+        public IActionResult GetStateBy(string stateName)
+        {
+            return Ok(ResponseViewModel.Ok(_stateAppService.GetStateByIts(stateName)));
         }
     }
 }
