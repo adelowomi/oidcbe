1ee3df612153ac192dfb063e98334052133dc3bb
Osinnowo Emmanuel <liquidcoding2009@gmail.com>
2020-10-30 18:02:33 +0100

    Vendor Controller

--- WebAPI/Controllers/VendorsController.cs
+++ WebAPI/Controllers/VendorsController.cs
@@ -1,45 +1,121 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Security.Claims;
 using System.Threading.Tasks;
+using AppService.AppModel.InputModel;
+using AppService.AppModel.ViewModel;
 using AppService.Repository.Abstractions;
+using Core.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace WebAPI.Controllers
 {
+    [Authorize(Policy = "PlotPolicy")]
     public class VendorsController : ControllerBase
     {
-        private IVendorAppService _vendorAppService;
+        private readonly IUserService _userService;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public VendorsController (IVendorAppService vendorAppService)
+        public VendorsController(IUserService userService, UserManager<AppUser> userManager, IHttpContextAccessor httpContextAccessor)
         {
-            _vendorAppService = vendorAppService;
+            _userService = userService;
+            _userManager = userManager;
+            _httpContextAccessor = httpContextAccessor;
         }
 
-        [HttpGet]
-        [Route("api/vendor")]
-        public IActionResult GetVendorById(int vendorId)
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("api/vendor/auth")]
+        public async Task<IActionResult> GetTokenAsync([FromBody] LoginInputModel model)
         {
             if (!ModelState.IsValid)
             {
                 return BadRequest();
             }
 
-            return Ok(_vendorAppService.GetVendorById(vendorId));
+            return Ok(await _userService.AuthenticateAsync(model));
         }
 
-        [HttpGet]
-        [Route("api/vendor/{email}")]
-        public IActionResult GetVendorByEmail(string emailAddress)
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("api/vendor/register")]
+        public async Task<IActionResult> RegisterAsync([FromBody] RegisterInputModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ResponseViewModel.Error("Validation error, please enter the require fields"));
+            }
+
+            return Ok(await _userService.RegisterAsync(model));
+        }
+
+        [AllowAnonymous]
+        [HttpPut]
+        [Route("api/vendor/profile/completion")]
+        public async Task<IActionResult> ProfileUpdate([FromBody] UserInputModel model)
         {
             if (!ModelState.IsValid)
             {
                 return BadRequest();
             }
 
-            return Ok(_vendorAppService.GetVendorByEmail(emailAddress));
+            return Ok(await _userService.UpdateAsync(model));
+        }
+
+
+        [HttpGet]
+        [Route("api/vendor/reset-password")]
+        public async Task<IActionResult> GetResetToken(string email)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var result = await _userService.ResetPasswordAsync(email);
+
+            if (result.Status)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+
+        [AllowAnonymous]
+        [HttpPut]
+        [Route("api/vendor/complete-reset")]
+        public async Task<IActionResult> CompleteResetAsync([FromBody] CompleteForgotPasswordInputModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var result = await _userService.CompleteResetPasswordAsync(model);
+
+            if (result.Status)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+
+        [HttpPut]
+        [Route("api/vendor/change-password")]
+        public async Task<IActionResult> ChangePasswordAsync([FromBody] ChangePasswordInputModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var result = await _userService.ChangePasswordAsync(model);
+
+            return Ok(result);
         }
     }
 }
