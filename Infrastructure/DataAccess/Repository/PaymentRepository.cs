9e5f302c5902fb18e925eb0752ecd98924bb36ab
Osinnowo Emmanuel <liquidcoding2009@gmail.com>
2020-12-14 13:37:48 +0100

    Included Plot --> Lazy Loader

--- Infrastructure/DataAccess/Repository/PaymentRepository.cs
+++ Infrastructure/DataAccess/Repository/PaymentRepository.cs
@@ -85,6 +85,7 @@ namespace Infrastructure.DataAccess.Repository
                 .Include(x => x.PaymentStatus)
                 .Include(x => x.Subscription)
                 .Include(x => x.Subscription.Offer)
+                .Include(x => x.Subscription.Offer.Plot)
                 .Include(x => x.Subscription.SubscriptionStatus);
             return result;
         }
