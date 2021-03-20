ce267858436c16f1ac59a2500d1d20e52548597f
Osinnowo Emmanuel <liquidcoding2009@gmail.com>
2021-03-20 02:20:29 +0100

    AccountController

--- AppService/AppModel/InputModel/AccountInputModel.cs
+++ AppService/AppModel/InputModel/AccountInputModel.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace AppService.AppModel.InputModel
 {
     public class AccountInputModel
     {
+        [Required]
         public string BankName { get; set; }
 
+        [Required]
         public string AccountName { get; set; }
 
+        [Required]
         public string AccountNumber { get; set; }
     }
 }
---
+++ WebAPI/Controllers/AccountsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using AppService.AppModel.InputModel;
+using AppService.Repository.Abstractions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Authorize]
+    public class AccountsController : ControllerBase
+    {
+        private readonly IUtilityAppService _utilityAppService;
+
+        public AccountsController(IUtilityAppService utilityAppService)
+        {
+            _utilityAppService = utilityAppService;
+        }
+
+        [HttpGet]
+        [Route("api/accounts")]
+        public IActionResult GetAccounts ()
+        {
+            return Ok(_utilityAppService.GetAccounts());
+        }
+
+        [HttpPost]
+        [Route("api/account")]
+        public IActionResult PostAccount([FromBody] AccountInputModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            return Ok(_utilityAppService.CreateNewAccount(model));
+        }
+    }
+}
