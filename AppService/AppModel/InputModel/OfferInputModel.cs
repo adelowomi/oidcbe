b5c826e246aaf0c9a42108fdfa452e83be9da338
adelowomi <adelowo_a@bts.com.ng>
2021-04-07 04:51:07 +0100

    rmoved temboo using

--- AppService/AppModel/InputModel/OfferInputModel.cs
+++ AppService/AppModel/InputModel/OfferInputModel.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 using Core.Model;
-using Temboo.Library.Google.BigQuery.TableData;
 
 namespace AppService.AppModel.InputModel
 {
